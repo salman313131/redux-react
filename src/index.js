978584554232689e125f41cbdc743b6159f9ee54
salman <salmankhan.faze@gmail.com>
2023-11-12 15:01:56 +0530

    Subscribing and Dispatching Actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import store from './store/index';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
